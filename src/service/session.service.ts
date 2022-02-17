8e3372e5bc6a2ed9cc27bfcb5349a109536e139e
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-02-17 21:22:38 +0100

    updating and reIssuing access tokens

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -14,3 +14,36 @@ export const createSession = async (userId: string, userAgent: string) => {
 export const findSessions = async (query: FilterQuery<SessionDocument>) => {
     return SessionModel.find(query).lean();
 };
+
+export const updateSession = async (
+    query: FilterQuery<SessionDocument>,
+    update: UpdateQuery<SessionDocument>
+) => {
+    return SessionModel.updateOne(query, update);
+};
+
+export const reIssueAccessToken = async ({
+    refreshToken,
+}: {
+    refreshToken: string;
+}) => {
+    const { decoded } = verifyJwt(refreshToken, "refreshTokenPublicKey");
+
+    if (!decoded || !get(decoded, "session")) return false;
+
+    const session = await SessionModel.findById(get(decoded, "session"));
+
+    if (!session || !session.valid) return false;
+
+    const user = await findUser({ _id: session.user });
+
+    if (!user) return false;
+
+    const accessToken = signJwt(
+        { ...user, session: session._id },
+        "accessTokenPrivateKey",
+        { expiresIn: config.get("accessTokenTtl") } // 15 minutes
+    );
+
+    return accessToken;
+};
