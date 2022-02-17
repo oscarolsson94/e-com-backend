c7ad4b601305437c9e528a385306f5e19760ba81
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-02-17 21:05:42 +0100

    hashing and comparing hashed passwords functions

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -24,3 +24,29 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre("save", async function (next) {
+    let user = this as UserDocument;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+
+    const hash = bcrypt.hashSync(user.password, salt);
+
+    user.password = hash;
+
+    return next();
+});
+
+userSchema.methods.comparePassword = async function (
+    candidatePassword: string
+): Promise<boolean> {
+    const user = this as UserDocument;
+
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+export const UserModel = mongoose.model<UserDocument>("User", userSchema);
