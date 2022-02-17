a3b73209f189e1e4429a064ff6928578af3de51e
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-02-17 21:19:00 +0100

    product routes

--- src/routes.ts
+++ src/routes.ts
@@ -78,3 +78,52 @@ function routes(app: Express) {
     app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
     app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+    app.post(
+        "/api/products",
+        [requireUser, validateResource(createProductSchema)],
+        createProductHandler
+    );
+
+    /**
+     * @openapi
+     * '/api/products/{productId}':
+     *  get:
+     *     tags:
+     *     - Products
+     *     summary: Get a single product by the productId
+     *     parameters:
+     *      - name: productId
+     *        in: path
+     *        description: The id of the product
+     *        required: true
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *          application/json:
+     *           schema:
+     *              $ref: '#/components/schema/Product'
+     *       404:
+     *         description: Product not found
+     */
+    app.put(
+        "/api/products/:productId",
+        [requireUser, validateResource(updateProductSchema)],
+        updateProductHandler
+    );
+
+    app.get(
+        "/api/products/:productId",
+        validateResource(getProductSchema),
+        getProductHandler
+    );
+
+    app.delete(
+        "/api/products/:productId",
+        [requireUser, validateResource(deleteProductSchema)],
+        deleteProductHandler
+    );
+}
+
+export default routes;
