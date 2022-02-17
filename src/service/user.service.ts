ac9e2ab8fba406e01ce10081df7d1a9f702c264c
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-02-17 21:20:18 +0100

    validating password and findUser util

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -11,3 +11,27 @@ export const createUser = async (input: UserInput) => {
         throw new Error(e);
     }
 };
+
+export const validatePassword = async ({
+    email,
+    password,
+}: {
+    email: string;
+    password: string;
+}) => {
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+        return false;
+    }
+
+    const isValid = await user.comparePassword(password);
+
+    if (!isValid) return false;
+
+    return omit(user.toJSON(), "password");
+};
+
+export const findUser = async (query: FilterQuery<UserDocument>) => {
+    return UserModel.findOne(query).lean();
+};
